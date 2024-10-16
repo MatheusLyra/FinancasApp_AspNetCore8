1d05ba6f97fb4c81a400150fa5a46ccbfe730d83
MatheusLyra <mdlimalyra@gmail.com>
2024-10-15 22:10:44 -0300

    Implementado estrutura de autenticação, interface, service e end-ponts de conta e categoria.

--- FinancasApp.Domain/Services/UsuarioDomainService.cs
+++ FinancasApp.Domain/Services/UsuarioDomainService.cs
@@ -3,6 +3,7 @@ using FinancasApp.Domain.Dtos.Responses;
 using FinancasApp.Domain.Entities;
 using FinancasApp.Domain.Helpers;
 using FinancasApp.Domain.Interfaces.Repositories;
+using FinancasApp.Domain.Interfaces.Security;
 using FinancasApp.Domain.Interfaces.Services;
 using System;
 using System.Collections.Generic;
@@ -14,11 +15,15 @@ namespace FinancasApp.Domain.Services
 {
     public class UsuarioDomainService : IUsuarioDomainService
     {
+        //atributos
         private readonly IUsuarioRepository _usuarioRepository;
+        private readonly ITokenSecurityService _tokenSecurityService;
 
-        public UsuarioDomainService(IUsuarioRepository usuarioRepository)
+        //método construtor para injeção de dependência (inicialização) do atributo
+        public UsuarioDomainService(IUsuarioRepository usuarioRepository, ITokenSecurityService tokenSecurityService)
         {
             _usuarioRepository = usuarioRepository;
+            _tokenSecurityService = tokenSecurityService;
         }
 
         public AutenticarUsuarioResponseDto Autenticar(AutenticarUsuarioRequestDto dto)
@@ -37,10 +42,9 @@ namespace FinancasApp.Domain.Services
                 Nome = usuario.Nome,
                 Email = usuario.Email,
                 DataHoraAcesso = DateTime.Now,
-                DataHoraExpiracao = null, //falta gerar
-                Token = null //falta gerar
+                DataHoraExpiracao = DateTime.Now.AddHours(1),
+                Token = _tokenSecurityService.CreateToken(usuario)
             };
-
         }
 
         public CriarUsuarioResponseDto Criar(CriarUsuarioRequestDto dto)
@@ -68,10 +72,10 @@ namespace FinancasApp.Domain.Services
                 Nome = usuario.Nome,
                 Email = usuario.Email,
                 DataHoraCadastro = DateTime.Now
-
             };
         }
     }
 }
+
 
 
